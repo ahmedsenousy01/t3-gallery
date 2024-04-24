8155ed90ab6c5b9eb3afe1a1ac8e7a93e7f74362
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2024-04-24 15:30:14 +0200

    added auth to home page

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = () => (
   <nav className="">
-    <div className="flex flex-wrap items-center justify-between bg-gray-800 p-4">
+    <div className="fixed left-0 right-0 top-0 flex flex-wrap items-center justify-between bg-gray-800 p-4">
       <div className="mr-6 flex flex-shrink-0 items-center text-white">
         <Link href="/">
           <h1 className="text-3xl font-bold tracking-tight">Gallery</h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,48 @@
 import { db } from "~/server/db";
 import Image from "next/image";
 import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 // TODO: learn about the export const (configurationOptions) like this one
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany();
 
   return (
+    <div className="mx-auto mt-24 flex w-[95%] flex-wrap gap-x-4 gap-y-8">
+      {[...images, ...images, ...images].map((img, index) => (
+        <div
+          key={img.id + "-" + index}
+          className="flex w-52 flex-col justify-between gap-4"
+        >
+          <Link href={`/post/${img.id}`} className="h-[12vh] max-h-64 min-h-32">
+            <Image
+              width={300}
+              height={300}
+              src={img.url}
+              alt={img.id.toString()}
+              className="size-full"
+            />
+          </Link>
+          <span>{img.name}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="mt-12 flex flex-wrap gap-x-4 gap-y-8">
-        {[...images, ...images, ...images].map((img, index) => (
-          <div
-            key={img.id + "-" + index}
-            className="flex w-52 flex-col justify-between gap-4"
-          >
-            <Link
-              href={`/post/${img.id}`}
-              className="h-[12vh] max-h-64 min-h-32"
-            >
-              <Image
-                width={300}
-                height={300}
-                src={img.url}
-                alt={img.id.toString()}
-                className="size-full"
-              />
-            </Link>
-            <span>{img.name}</span>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="flex h-screen flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold">Welcome to the Gallery!</h1>
+          <p className="text-lg">Please sign in to view the gallery.</p>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
